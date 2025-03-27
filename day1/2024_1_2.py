38d998886924a4b9d79a04f02bb2409efd61241a
PatrycjaD <domanska.patrycja7@gmail.com>
2025-03-27 19:25:46 +0100

    refactor: day1_2 implemented collections counter

--- day1/2024_1_2.py
+++ day1/2024_1_2.py
@@ -1,31 +1,21 @@
-def load_data_from_file(data_file):
-    return open(data_file,'r').read()
-
-def split_data_into_lists(raw_data):
-    tab1, tab2 = [],[]
-    line_data = raw_data.split('\n')
-    for trace in line_data:
-        split_trace = trace.split('   ')
-        if len(split_trace)>0:
-            tab1.append(int(split_trace[0]))
-            tab2.append(int(split_trace[1]))
-        else:
-            tab1.append(int(split_trace[0]))
-    return tab1, tab2
+from load_data_2024_1 import prepare_data
+from collections import Counter
 
 def calc_similarity(l_tab, similar_tab):
-    res_similarity=[]
-    for nb in l_tab:
-        res_similarity.append(nb*similar_tab.count(nb))
+    counter_dict=Counter(similar_tab)
+    res_similarity=0
+
+    for nb in set(l_tab):
+        res_similarity += nb*counter_dict[nb]
     return res_similarity
 
 def main(data_file):
-    raw_data = load_data_from_file(data_file)
-    tab1, tab2 = split_data_into_lists(raw_data)
-    sim_tab = calc_similarity(tab1,tab2)
-    return sum(sim_tab)
-
+    tab_left, tab_right = prepare_data(data_file)
+    return calc_similarity(tab_left,tab_right)
+     
 if __name__ == '__main__':
-    # print(main('data_test.txt'))
-    print(main('data1.txt'))
+    try:
+        print(main('day1/data1.txt'))
+    except:
+        print(main('data1.txt'))
     #Right answer: 24931009
