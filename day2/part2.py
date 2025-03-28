37cdf2e30b213f6ea923bc3649700f6bd7476033
PatrycjaD <domanska.patrycja7@gmail.com>
2025-03-28 13:09:48 +0100

    fix: working - brute force

--- day2/part2.py
+++ day2/part2.py
@@ -4,15 +4,13 @@ def my_diff_function(data_trace):
     return [data_trace[i]-data_trace[i+1] for i in range(len(data_trace)-1)]
 
 def if_increase_decrease(data_trace):
-    diff = my_diff_function(data_trace)
-    if (bugs:=len([x for x in diff if x<=0]))<=1: #for decrease
-        if (bugs:=bugs+len([i for i in range(1,len(data_trace)-1) if data_trace[i-1]<data_trace[i+1]]))<=1:
-            if (bugs:=bugs+len([x for x in diff if x>3]))<=1:
-                return True
-    bugs=0
-    if (bugs:=len([x for x in diff if x>=0]))<=1: # for increase
-        if (bugs:=bugs+len([i for i in range(1,len(data_trace)-1) if data_trace[i-1]>data_trace[i+1]]))<=1:
-            if (bugs:=bugs+len([x for x in diff if x<(-3)]))<=1:
+
+    for i in range(len(data_trace)):
+        temp_list= data_trace[:i]+data_trace[i+1:]
+
+        if temp_list==sorted(temp_list) or temp_list==sorted(temp_list, reverse=True):
+            diff_list = my_diff_function(temp_list)
+            if all(-3 <= x <= 3 and x != 0 for x in diff_list):
                 return True
     return False
 
@@ -22,7 +20,6 @@ def main(data_file):
     safe = 0
 
     for data_trace in data_matrix:
-
         res = if_increase_decrease(data_trace) #check if our data traces are fully increasing or deceasing
         if res==True:
             safe+=1
@@ -32,5 +29,5 @@ def main(data_file):
 if __name__ == '__main__':
     # test_function('problem_test.txt')
     # print(main('special_cases.txt'))
-    print(main('problem_test.txt'))
-    # print(main('data2.txt'))
+    # print(main('problem_test.txt'))
+    print(main('data2.txt'))
